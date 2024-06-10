7dafed1e22be1cc8bd5a564e1118501c58bd3517
226720 <Fiona.Chacko@ust.com>
2024-06-10 14:16:01 +0530

    initial check

--- src/main/java/com/ust/entity/Student.java
+++ src/main/java/com/ust/entity/Student.java
@@ -1,6 +1,14 @@
 package com.ust.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+	
 	private String studentId;
 	private String studentName;
 	
